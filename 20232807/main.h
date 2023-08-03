4d3d754d9bdec7408ddf517caa8328502ee33d5c
Vhvo-2004 <120145024+Vhvo-2004@users.noreply.github.com>
2023-08-03 02:10:34 -0400

    Add files via upload

---
+++ 20232807/main.h
@@ -0,0 +1,42 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+typedef struct NoArvore
+{
+    char dado;
+    struct NoArvore *esquerda;
+    struct NoArvore *direita;
+} NoArvore;
+
+typedef struct Arvore
+{
+    NoArvore *raiz;
+} Arvore;
+
+typedef struct NoPilha
+{
+    NoArvore *dado;
+    struct NoPilha *proximo;
+} NoPilha;
+
+typedef struct Pilha
+{
+    NoPilha *topo;
+} Pilha;
+
+Pilha *criar_pilha();
+void empilhar(Pilha *pilha, NoArvore *dado);
+bool vazia(Pilha *pilha);
+NoArvore *desempilhar(Pilha *pilha);
+NoArvore *topo(Pilha *pilha);
+NoArvore *criar_no(char dado);
+NoArvore *construir_arvore(char *posfixa);
+void inserir(Arvore *a, char dado);
+void liberar_arvore(NoArvore *no);
+int prioridade(char op);
+void infixa_para_prefixa(char *infixa, char *prefixa);
+void infixa_para_posfixa(char *infixa, char *posfixa);
+void inverter(char *str);
+int avaliar(NoArvore *no);
+
+#endif
